55d3611a1a1711acbb8f582c19443ec5c9e07f1f
lrmendes <lrmen14@gmail.com>
2020-11-11 08:50:07 +0000

    UPDATE -> Graph2 & Graph1 (Download Button + Statistic Table)

--- apps/basic1.py
+++ apps/basic1.py
@@ -4,6 +4,7 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+from datetime import date, datetime
 
 from app import app
 
@@ -13,6 +14,13 @@ df = pd.read_csv('generated_dataset_article.csv', dayfirst=True, parse_dates=['t
 df['isRain'] = df['isRain'].astype('bool')
 df['isHoliday'] = df['isHoliday'].astype('bool')
 df['isWeekday'] = df['isWeekday'].astype('bool')
+
+min_date = df['timeFrom'].min()
+max_date = df['timeFrom'].max()
+start_min_date = min_date
+if len(df['timeFrom']) > 5000:
+    start_min_date = df.iloc[len(df)-5000]['timeFrom']
+
 #df['Legend'] = 1*df['isWeekday'] + 2*df['isRain'] + 4*df['isHoliday']
 
 #df['timeTo'] = pd.DatetimeIndex(df['timeTo']).time
@@ -29,39 +37,96 @@ df['isWeekday'] = df['isWeekday'].astype('bool')
 #)
 
 
-layout = dbc.Row([
-    dbc.Col([
-        dbc.Label('Choose Legend Filter'),
-        dcc.Dropdown(
-            id='legend-filter',
-            options=[
-                {'label': 'Rain Days', 'value': 'isRain'},
-                {'label': 'Holidays', 'value': 'isHoliday'},
-                {'label': 'WeekDays', 'value': 'isWeekday'},
-            ],
-            value='isRain',
-            clearable=False,
-            placeholder='Select Filter...'
-        ),
-        html.Br(),
-        dcc.Graph(id='mainchart')
+layout = html.Div([
+    dbc.Row([
+        dbc.Col([
+            html.Div([
+                dbc.Label('Date Range:'),
+                html.Br(),
+                dcc.DatePickerRange(
+                    id='b1_date_range',
+                    min_date_allowed=min_date,
+                    max_date_allowed=max_date,
+                    initial_visible_month=max_date,
+                    start_date=start_min_date,
+                    end_date=max_date,
+                ),
+            ]),
+        ], width="auto"),
+        dbc.Col([
+            dbc.Label('Choose Legend Filter'),
+            dcc.Dropdown(
+                id='b1_legend',
+                options=[
+                    {'label': 'Rain Days', 'value': 'isRain'},
+                    {'label': 'Holidays', 'value': 'isHoliday'},
+                    {'label': 'WeekDays', 'value': 'isWeekday'},
+                ],
+                value='isRain',
+                clearable=False,
+                placeholder='Select Filter...'
+            ),
+        ]),
     ]),
-],id='graph-grid')
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            dcc.Loading(id="loading-icon", children=[html.Div(dcc.Graph(id='b1_mainchart'))], type="default")
+        ], width=9),
+        dbc.Col([
+            html.Div([], id='b1_table_statistics'),
+        ])
+    ]),
+], id='graph-grid')
+
 
 @app.callback(
-    Output('mainchart','figure'),
-    Input('legend-filter','value')
+    [Output('b1_mainchart','figure'),
+     Output('b1_table_statistics', 'children')],
+    [Input('b1_legend','value'),
+     Input('b1_date_range','start_date'),
+     Input('b1_date_range','end_date')]
 )
+def update_data(legenddropval, start_date, end_date):
+    df2 = df.copy()
 
-def update_data(legenddropval):
-    mainchart = px.scatter(df, x="timeFrom", y="timeTo", height=700, hover_data=['isRain', 'isHoliday', 'isWeekday'], color=legenddropval)
-    mainchart.update_layout(
+    # Apply Date Range
+    date_mask = (df2['timeFrom'] >= start_date) & (df2['timeFrom'] <= end_date)
+    df2 = df2.loc[date_mask]
+
+    # Apply Legend
+    b1_mainchart = px.scatter(df2, x="timeFrom", y="timeTo", height=700, custom_data=['isRain','isHoliday','isWeekday'], color=legenddropval)
+    b1_mainchart.update_layout(
         title="All Parkings by Entrance & Exit Date/Times",
         xaxis_title="Entrance",
         yaxis_title="Exit",
         legend_title=legenddropval,
-        font=dict(
-            size=14,
-        )
     )
-    return (mainchart)
+    b1_mainchart.update_traces(
+        hoverinfo='skip',
+        hovertemplate='Entrance: <b>%{x}</b><br>'
+                      + 'Exit: <b>%{y}</b><br><br>'
+                      + 'isRain: <b>%{customdata[0]}</b><br>'
+                      + 'isHoliday: <b>%{customdata[1]}</b><br>'
+                      + 'isWeekday: <b>%{customdata[2]}</b>'
+                      + '<extra></extra>')
+
+    # Total Records
+    total_records = len(df2)
+
+    # Daily Mean Records
+    delta = pd.to_datetime(end_date).date() - pd.to_datetime(start_date).date()
+    b1_daily_mean = html.Div('Daily Mean Records: ' + str(round((total_records / delta.days), 2)) + ' parkings/day')
+
+    # Rainy & Sunny Records
+
+    statistics = pd.DataFrame(
+        {
+            "Statistic": ['Total Records: ','Daily Average Records: ', 'Sunny Weather Records: ','Rainy Weather Records: '],
+            "Value": [str(total_records), str(round((total_records / delta.days), 2)) + ' parkings/day', str(len(df2[df2['isRain'] == 0])), str(len(df2[df2['isRain'] == 1]))]
+        }
+    )
+
+    b1_table_statistics = dbc.Table.from_dataframe(statistics, bordered=True, dark=True, hover=True, responsive=True, striped=True)
+
+    return (b1_mainchart, b1_table_statistics)
--- apps/basic2.py
+++ apps/basic2.py
@@ -2,10 +2,17 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
+from dash import callback_context
 import pandas as pd
 import plotly.express as px
+import plotly.figure_factory as ff
 
 from app import app
+
+from datetime import datetime
+
+from dash_extensions import Download
+from dash_extensions.snippets import send_data_frame
 
 pd.options.plotting.backend = "plotly"
 
@@ -16,33 +23,95 @@ df['isRain'] = df['isRain'].astype('bool')
 df['isHoliday'] = df['isHoliday'].astype('bool')
 df['isWeekday'] = df['isWeekday'].astype('bool')
 
-layout = dbc.Row([
-    dbc.Col([
-        dbc.Label('Group Parkings By:'),
-        dcc.Dropdown(
-            id='b2_legend_filter',
-            options=[
-                {'label': 'Hours', 'value': 'hour'},
-                {'label': 'Days', 'value': 'day'},
-                {'label': 'Months', 'value': 'month'},
-            ],
-            value='day',
-            clearable=False,
-            placeholder='Select Filter...'
-        ),
-        html.Br(),
-        dcc.Graph(id='b2_mainchart')
-    ]),
-], id='graph-grid')
+min_date = df.index.min()
+max_date = df.index.max()
+start_min_date = min_date
+if len(df.index) > 5000:
+    start_min_date = df.index[len(df)-5000]
 
+layout = html.Div([
+    html.H4('Filters:'),
+    dbc.Row([
+        dbc.Col([
+            html.Div([
+                dbc.Label('Date Range:'),
+                html.Br(),
+                dcc.DatePickerRange(
+                    id='b2_date_range',
+                    min_date_allowed=min_date,
+                    max_date_allowed=max_date,
+                    initial_visible_month=max_date,
+                    start_date=start_min_date,
+                    end_date=max_date,
+                ),
+            ]),
+        ], id='graph-grid', width="auto"),
+        dbc.Col([
+            html.Div([
+                dbc.Label('Group Parkings By:'),
+                dcc.Dropdown(
+                    id='b2_legend_filter',
+                    options=[
+                        {'label': 'Hours', 'value': 'hour'},
+                        {'label': 'Days', 'value': 'day'},
+                        {'label': 'Months', 'value': 'month'},
+                    ],
+                    value='day',
+                    clearable=False,
+                    placeholder='Select Filter...'
+                ),
+            ]),
+        ], id='graph-grid', width="auto"),
+        dbc.Col([
+            html.Div([
+                dbc.Label('Graph Type:'),
+                dcc.Dropdown(
+                    id='b2_graph_type',
+                    options=[
+                        {'label': 'Default', 'value': 'default'},
+                        {'label': 'Distplot', 'value': 'dist'},
+                        {'label': 'Normal Distribution', 'value': 'normal'},
+                    ],
+                    value='default',
+                    clearable=False,
+                    placeholder='Select Graph Type...'
+                ),
+            ]),
+        ], id='graph-grid'),
+    ]),
+    html.Br(),
+    html.H4('Data Visualization:'),
+    dbc.Row([
+        dbc.Col([
+            html.Br(),
+            dcc.Loading(id="loading-icon",children=[html.Div(dcc.Graph(id='b2_mainchart'))], type="default")
+        ], width=9, id='graph-grid'),
+        dbc.Col([
+            html.Div([], id='b2_table_statistics'),
+            html.Div([html.Button("Download CSV", id="b2_btn_download_csv"), Download(id="b2_download_csv")])
+        ], id='statistics-grid'),
+    ])
+])
 
 @app.callback(
-    Output('b2_mainchart', 'figure'),
-    Input('b2_legend_filter', 'value')
+    [Output('b2_mainchart', 'figure'),
+     Output('b2_table_statistics', 'children'),
+     Output("b2_download_csv", "data")],
+    [Input('b2_legend_filter', 'value'),
+     Input('b2_date_range','start_date'),
+     Input('b2_date_range','end_date'),
+     Input('b2_graph_type','value'),
+     Input("b2_btn_download_csv", "n_clicks")]
 )
-def update_data(legenddropval):
+
+def update_data(legenddropval,start_date,end_date, b2_graph_type,b2_btn_download_csv):
     df2 = df.copy()
 
+    # Apply Date Range
+    date_mask = (df2.index >= start_date) & (df2.index <= end_date)
+    df2 = df2.loc[date_mask]
+
+    # Apply Legends
     if (legenddropval == 'hour'):
         df2['TotalParkings'] = 1
         pd.to_datetime(df2.index, errors='ignore')
@@ -65,21 +134,42 @@ def update_data(legenddropval):
     df2['isHoliday'] = df2.apply((lambda x: 1 if x['isHoliday'] > 0 else 0), axis=1)
     df2['isHoliday'] = df2['isHoliday'].astype('bool')
 
-    mainchart = px.line(df2, x=df2.index, y='TotalParkings', custom_data=['isWeekday',
-                                                                          'isHoliday'])  # hover_name=df2.index, hover_data=['isWeekday','isHoliday'])
-    mainchart.update_layout(
-        title="All Parkings Grouped By " + legenddropval,
-        xaxis_title="Date",
-        yaxis_title="Total Parkings",
-        font=dict(
-            size=14,
+    if b2_graph_type == 'dist':
+        mainchart = ff.create_distplot([df2['TotalParkings']], ['Distplot'])
+    elif b2_graph_type == 'normal':
+        mainchart = ff.create_distplot([df2['TotalParkings']], ['Distplot'], curve_type='normal')
+    else:
+        mainchart = px.line(df2, x=df2.index, y='TotalParkings', custom_data=['isWeekday', 'isHoliday'])
+        mainchart.update_layout(
+            title="All Parkings Grouped By " + legenddropval,
+            xaxis_title="Date",
+            yaxis_title="Total Parkings",
+            font=dict(
+                size=14,
+            )
         )
-    )
-    mainchart.update_traces(
-        mode='lines+markers',
-        hovertemplate='<b>%{x}</b><br><br>'
-                      + '<b>Total Parkings: %{y}</b><br><br>'
-                      + 'isWeekday: <b>%{customdata[0]}</b><br>'
-                      + 'isHoliday: <b>%{customdata[1]}</b>')
+        mainchart.update_traces(
+            mode='lines+markers',
+            hovertemplate='<b>%{x}</b><br><br>'
+                          + '<b>Total Parkings: %{y}</b><br><br>'
+                          + 'isWeekday: <b>%{customdata[0]}</b><br>'
+                          + 'isHoliday: <b>%{customdata[1]}</b>')
 
-    return (mainchart)
+    total_records = len(df2)
+
+    statistics = pd.DataFrame(
+        {
+            "Statistic": ['Total Records: ','Average Parkings'],
+            "Value": [str(total_records), str(round(df2['TotalParkings'].mean(), 2))]
+        }
+    )
+
+    b2_table_statistics = dbc.Table.from_dataframe(statistics, bordered=True, dark=True, hover=True, responsive=True, striped=True)
+
+    # Check if Download Button has fired
+    download_file = None
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+    if 'b2_btn_download_csv' in changed_id:
+        download_file = send_data_frame(df2.to_csv, filename=datetime.now().strftime("%Y-%m-%d_%H-%M-%S")+".csv")
+
+    return (mainchart, b2_table_statistics, download_file)
